4ca507640fc76833412aa9cf16a8984aade66421
almerindo <almerindo.rehem@gmail.com>
2023-03-14 18:00:48 -0300

    refactor: lint

--- src/easyOpenAI/core/repository/interfaces/message.repository.interface.ts
+++ src/easyOpenAI/core/repository/interfaces/message.repository.interface.ts
@@ -1,9 +1,10 @@
 import { IChatCompletionMessage } from '../..';
 
 export interface IMessageRepository {
-  addMessage(params: IChatCompletionMessage): Promise<void>;
+  addMessage(params: IChatCompletionMessage): Promise<IChatCompletionMessage>;
   getMessages(
     ownerId: string,
+    chatId: string,
     params?: {
       skip: number;
       limit: number;
