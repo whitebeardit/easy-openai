61d87f4f0cb4a2efb9f38504a5507a25c0959a8d
Almerindo Rehem <almerindo.rehem@gmail.com>
2023-03-26 00:17:06 -0300

    chore: remove unnecessaries jest files

--- jest/start-integration.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-async function globalSetup() {
-    console.info('globalSetup()');
-}
-exports.default = globalSetup;
-//# sourceMappingURL=start-integration.js.map
--- jest/stop-integration.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-async function globalTeardown() {
-    console.info('globalTeardown()');
-}
-exports.default = globalTeardown;
-//# sourceMappingURL=stop-integration.js.map
